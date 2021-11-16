1579f404827329f881ededc6add9171eaf2a313c
dacitto <dacidigital@gmail.com>
2021-11-16 16:59:56 +0100

    add Weather and WeatherCard components and fetch data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import { useQuery } from "react-query";
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Weather from "./components/Weather/Weather";
+const fetchWeather = async (key) => {
+  const city = key.queryKey[1];
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+  );
+  return res.json();
+};
 
 function App() {
+  const [city, setCity] = useState("djelfa");
+  const { data, status } = useQuery(["city", city], fetchWeather);
   return (
     <div>
-      <h1>TEST</h1>
+      <Header setCity={setCity} city={city}></Header>
+      {status === "loading" && <h3>Loading</h3>}
+      {status === "success" && data.name && (
+        <Weather city={city} datas={data}></Weather>
+      )}
     </div>
   );
 }

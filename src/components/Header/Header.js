0a5ee5a1fbda919d2df149977cd87aa77a6a40d2
dacitto <dacidigital@gmail.com>
2021-11-21 10:25:20 +0100

    feat: add search button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import { BsSearch } from "react-icons/bs";
 const Header = ({ setCity, city }) => {
+  const [search, setSeach] = useState("");
   return (
     <header className="header">
       <div className="container">
         <h1 className="title">weatherly</h1>
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Type city name here "
-            className="search-input"
-            value={city}
-            onChange={(e) => {
-              e.preventDefault();
-              setCity(e.target.value);
+        <div className="flex">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Type city name here "
+              className="search-input"
+              value={search}
+              onChange={(e) => {
+                e.preventDefault();
+                setSeach(e.target.value);
+              }}
+            ></input>
+            <BsSearch className="icon" />
+          </div>
+          <button
+            className="btn"
+            onClick={() => {
+              setCity(search);
             }}
-          ></input>
-          <BsSearch className="icon" />
+          >
+            Search
+          </button>
         </div>
       </div>
     </header>
